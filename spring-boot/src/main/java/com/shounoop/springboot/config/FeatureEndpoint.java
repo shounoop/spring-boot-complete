be3c24112444f2954a6174c27dcf005e2ebe6a7b
HMT <tuan.hm@tinasoft.com.vn>
2024-03-04 17:05:34 +0700

    spring-boot: spring boot actuator

---
+++ spring-boot/src/main/java/com/shounoop/springboot/config/FeatureEndpoint.java
@@ -0,0 +1,41 @@
+package com.shounoop.springboot.config;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.boot.actuate.endpoint.annotation.Endpoint;
+import org.springframework.boot.actuate.endpoint.annotation.ReadOperation;
+import org.springframework.boot.actuate.endpoint.annotation.Selector;
+import org.springframework.stereotype.Component;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+@Component
+@Endpoint(id = "feature")
+public class FeatureEndpoint {
+    private final Map<String, Feature> featureMap = new ConcurrentHashMap<>();
+
+    public FeatureEndpoint() {
+        featureMap.put("Department", new Feature(true));
+        featureMap.put("User", new Feature(false));
+        featureMap.put("Authentication", new Feature(false));
+    }
+
+    @ReadOperation
+    public Map<String, Feature> features() {
+        return featureMap;
+    }
+
+    @ReadOperation
+    public Feature feature(@Selector String name) {
+        return featureMap.get(name);
+    }
+
+    @Data
+    @NoArgsConstructor
+    @AllArgsConstructor
+    private static class Feature {
+        private boolean isEnabled;
+    }
+}
