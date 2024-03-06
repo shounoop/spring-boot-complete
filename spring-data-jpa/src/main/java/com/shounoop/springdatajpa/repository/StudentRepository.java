07ee5d9e4e77e85ff8471ed3eac101b62e66fdf0
HMT <tuan.hm@tinasoft.com.vn>
2024-03-06 09:43:43 +0700

    spring-data-jpa: transaction management in spring data

--- spring-data-jpa/src/main/java/com/shounoop/springdatajpa/repository/StudentRepository.java
+++ spring-data-jpa/src/main/java/com/shounoop/springdatajpa/repository/StudentRepository.java
@@ -1,7 +1,9 @@
 package com.shounoop.springdatajpa.repository;
 
 import com.shounoop.springdatajpa.entity.Student;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -34,4 +36,9 @@ public interface StudentRepository extends JpaRepository<Student, Long> {
     // Native Named Param
     @Query(value = "SELECT * FROM tbl_student s where s.email_address = :emailId", nativeQuery = true)
     Student getStudentByEmailAddressNativeNameParam(@Param("emailId") String emailId);
+
+    @Modifying
+    @Transactional
+    @Query(value = "update tbl_student set first_name = ?1 where  email_address = ?2", nativeQuery = true)
+    int updateStudentByEmailId(String firstName, String emailId);
 }
